fe24729f89248bc4f9827b2206984647df3b2d69
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-05 19:08:08 +0200

    update app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Greeting from './Greeting';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
+import Greeting from './components/greeting';
+import Home from './components/home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/greeting" element={<Greeting />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/greeting">Greeting</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+
+        <Routes>
+          <Route path="/home" exact element={<Home />} />
+          <Route path="/greeting" element={<Greeting />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
